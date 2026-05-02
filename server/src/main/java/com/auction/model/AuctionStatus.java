214698a4ba9002bd5290dbf61fb8a616f69a95a5
Nghia-clm <nghiavannguyen07@gmail.com>
2026-05-02 21:07:53 +0700

    del

--- server/src/main/java/com/auction/model/AuctionStatus.java
+++
@@ -1,9 +0,0 @@
-package com.auction.model;
-
-public enum AuctionStatus {
-    OPEN,      // mới tạo, chưa bắt đầu
-    RUNNING,   // đang diễn ra
-    FINISHED,  // đã kết thúc, có người thắng
-    PAID,      // người thắng đã thanh toán
-    CANCELED   // bị hủy
-}
